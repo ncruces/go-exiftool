814351b37edb6cb7417b3c57c02080920a805e8e
Nuno Cruces <ncruces@users.noreply.github.com>
2021-04-24 01:30:17 +0100

    Perl 5.32.1.1.

--- dist_windows.sh
+++ dist_windows.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 shopt -s extglob
 
 exiftool="https://exiftool.org/Image-ExifTool-12.16.tar.gz"
-strawberry="https://strawberryperl.com/download/5.32.0.1/strawberry-perl-5.32.0.1-64bit-portable.zip"
+strawberry="https://strawberryperl.com/download/5.32.1.1/strawberry-perl-5.32.1.1-64bit-portable.zip"
 
 cd $(dirname "${BASH_SOURCE[0]}")
 
@@ -21,15 +21,18 @@ curl "$strawberry" --output tmp/strawberry.zip
 unzip -qd tmp/strawberry/ tmp/strawberry.zip
 
 # Install modules
-pushd tmp/strawberry/perl/bin
-./cpanm.bat --notest Win32::FindFile
-popd
+PATH=`pwd`/tmp/strawberry/perl/bin:$PATH
+PATH=`pwd`/tmp/strawberry/c/bin:$PATH
+cpanm.bat --notest Win32::FindFile
 
 # Cleanup Strawberry
 pushd tmp/strawberry/perl
 rm -rf lib/CORE lib/CPAN?(.pm) lib/Pod
-rm -rf lib/Encode/+(CN|JP|KR|TW)?(.pm)
-rm -rf lib/auto/Encode/+(CN|JP|KR|TW)
+rm -rf lib/Module lib/Software lib/ExtUtils
+rm -rf lib/Devel lib/Test*
+rm -rf lib/auto/Devel lib/auto/Test*
+rm -rf lib/Unicode lib/Encode/+(CN|JP|KR|TW)?(.pm)
+rm -rf lib/auto/Unicode lib/auto/Encode/+(CN|JP|KR|TW)
 find lib -name '.packlist' -delete
 find lib -name '*.pod' -delete
 find lib -type d -empty -delete
@@ -41,7 +44,6 @@ cp -rl tmp/strawberry/perl/bin/perl.exe tmp/exiftool/exiftool.exe
 cp -rl tmp/strawberry/perl/bin/*.dll tmp/exiftool
 cp -rl tmp/strawberry/perl/bin/ tmp/exiftool
 cp -rl tmp/strawberry/perl/lib/ tmp/exiftool
-cp -rl tmp/strawberry/perl/vendor/lib/Archive/Zip* tmp/exiftool/lib/Archive
 cp -rl tmp/strawberry/perl/vendor/lib/Win32/API* tmp/exiftool/lib/Win32
 cp -rl tmp/strawberry/perl/vendor/lib/auto/Win32/API* tmp/exiftool/lib/auto/Win32
 cp -rl tmp/strawberry/perl/site/lib/Win32/FindFile* tmp/exiftool/lib/Win32
