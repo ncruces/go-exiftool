6475abc368ebf88d624fbc937b7e2f6e7bfa372e
Nuno Cruces <ncruces@users.noreply.github.com>
2021-09-02 12:56:22 +0100

    ExifTool 12.30.

--- dist_unix.sh
+++ dist_unix.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 shopt -s extglob
 
-exiftool="https://exiftool.org/Image-ExifTool-12.26.tar.gz"
+exiftool="https://exiftool.org/Image-ExifTool-12.30.tar.gz"
 
 
 cd $(dirname "${BASH_SOURCE[0]}")
